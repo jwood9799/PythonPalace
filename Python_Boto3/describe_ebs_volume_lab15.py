23da7abf76611fc2696f2834cea11703132be9fe
Jason Wood <jwood9799@gmail.com>
2022-08-08 14:36:21 +0000

    More Boto3

--- Python_Boto3/describe_ebs_volume_lab15.py
+++ Python_Boto3/describe_ebs_volume_lab15.py
@@ -8,7 +8,20 @@ response=ec2.describe_volumes()
 
 data=response["Volumes"]
 
-for volume in data:
-    volume_data=volume["Attachments"]
-    
-    
+with io.open("volume.csv","w",encoding="utf-8")as f1:
+    f1.write("device,volumeid,instanceid,size\n")
+    for volume in data:
+        
+     volume_data=volume["Attachments"]
+     if(len(volume_data)>0):
+         device=volume["Attachments"][0]["Device"]
+         instance_id=volume["Attachments"][0]["InstanceId"]
+         
+     else:
+        device="not attached"
+        instance_id="not attached"
+     volume_id=volume["VolumeId"]
+     size=volume["Size"]
+     f1.write(device+","+volume_id+","+instance_id+","+str(size)+"\n")
+         
+         
--- to-celsius.py
+++
@@ -1,8 +0,0 @@
-
-
-# Python implemention goes here
-
-fahrenheit = float(input("what temperature (in Fahrenheit) would you like converted to Celsius? "))
-celsius = (fahrenheit -32) * 5 / 9
-
-print(fahrenheit, "F is", round(celsius, 2), "C")
